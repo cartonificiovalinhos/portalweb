8c8f3a51f6d606d897106638d03c070ff7cbe3e3
PortalWeb <portalweb@example.com>
2026-04-03 20:54:44 -0300

    Ajusta output do build e migra middleware->proxy

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  distDir: '.next_build',
-  output: 'standalone',
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname, 'src');
     return config;
